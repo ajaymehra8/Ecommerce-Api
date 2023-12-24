85ccfb55e465a302866ef6d06b9db4eeb64a5d82
ajaymehra8 <116863739+ajaymehra8@users.noreply.github.com>
2023-12-24 15:53:57 +0530

    Updating loginController

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(morgan('dev'));
 // middleware
 // middleware
 app.use(cors({
-    origin: 'https://ecommerce-app-omega-livid.vercel.app',
+    origin: 'https://ecommerce-app-absr.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
