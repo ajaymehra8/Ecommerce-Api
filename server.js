739070022b9393a3c4bff830bd0ea6037a2b8522
ajaymehra8 <116863739+ajaymehra8@users.noreply.github.com>
2023-12-25 10:04:12 +0530

    Resolving some issue of cors

--- server.js
+++ server.js
@@ -1,54 +1,45 @@
-const dotenv=require("dotenv");
-const express= require('express');
-const colors= require("colors");
-const cors=require("cors");
-const morgan= require("morgan");
-const db=require('./config/db');
-const authRoutes=require("./routes/auth")
-const categoryRoutes=require("./routes/categoryRoutes");
-const productRoutes=require("./routes/productRoutes");
+const dotenv = require("dotenv");
+const express = require('express');
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const db = require('./config/db');
+const authRoutes = require("./routes/auth");
+const categoryRoutes = require("./routes/categoryRoutes");
+const productRoutes = require("./routes/productRoutes");
 
-//configure dotenv
+// Configure dotenv
 dotenv.config();
 
-//connect with database
+// Connect with the database
 db();
 
-//rest objects
-const app=express();
+// Create the express app
+const app = express();
 
-const PORT=process.env.PORT;
+// Define the PORT
+const PORT = process.env.PORT || 3000;
 
-//middleware
-app.use(cors());
-app.use(express.json());
-app.use(morgan('dev'));
-
-//cors policy
-// middleware
-// middleware
+// Middleware
 app.use(cors({
     origin: 'https://ecommerce-app-4mt6.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
+app.use(express.json());
+app.use(morgan('dev'));
 
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/products", productRoutes);
 
-
-//routes
-
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/category",categoryRoutes);
-app.use("/api/v1/products",productRoutes);
-
-
-//rest apis
- 
-app.get('/',(req,res)=>{
-    res.send("hellow");
-
+// Rest APIs
+app.get('/', (req, res) => {
+    res.send("Hello");
 });
 
-app.listen(PORT,()=>{
-    console.log(`App is listening at ${PORT}`.bgCyan.white);
-})
+// Start the server
+app.listen(PORT, () => {
+    console.log(`App is listening at http://localhost:${PORT}`.bgCyan.white);
+});
